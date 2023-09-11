77a82ee53285e700cf1b0bab6aae40b7f725a876
NLG Sakib <107909889+nnlgsakib@users.noreply.github.com>
2023-09-11 10:30:15 +0600

    Update bc.js

--- backend/bc.js
+++ backend/bc.js
@@ -7,57 +7,33 @@ const port = 4000;
 
 app.use(cors());
 
-app.get('/metrics', async (req, res) => {
-  try {
-    const prometheusURL = 'http://91.208.92.6:3000/';
-    const response = await axios.get(prometheusURL); 
-    res.send(response.data);
-  } catch (error) {
-    console.error('Error fetching Prometheus metrics:', error);
-    res.status(500).send('Error fetching Prometheus metrics');
-  }
-});
-app.get('/api/stats', async (req, res) => {
-  try {
-    const api = 'https://mainnet.mindscan.info/api/v2/stats';
-    const response = await axios.get(api); 
-    res.send(response.data);
-  } catch (error) {
-    console.error('Error fetching Prometheus metrics:', error);
-    res.status(500).send('Error fetching Prometheus metrics');
-  }
-});
-app.get('/api/txn', async (req, res) => {
-  try {
-    const txn = 'https://mainnet.mindscan.info/api/v2/main-page/transactions';
-    const response = await axios.get(txn); 
-    res.send(response.data);
-  } catch (error) {
-    console.error('Error fetching Prometheus metrics:', error);
-    res.status(500).send('Error fetching Prometheus metrics');
-  }
-});
-app.get('/api/txn/chart', async (req, res) => {
-  try {
-    const txn = 'https://mainnet.mindscan.info/api/v2/stats/charts/transactions';
-    const response = await axios.get(txn); 
-    res.send(response.data);
-  } catch (error) {
-    console.error('Error fetching Prometheus metrics:', error);
-    res.status(500).send('Error fetching Prometheus metrics');
-  }
-});
-app.get('/api/block', async (req, res) => {
-  try {
-    const block = 'https://mainnet.mindscan.info/api/v2/main-page/blocks';
-    const response = await axios.get(block); 
-    res.send(response.data);
-  } catch (error) {
-    console.error('Error fetching Prometheus metrics:', error);
-    res.status(500).send('Error fetching Prometheus metrics');
-  }
+// Define a reusable error handler function
+const handleError = (res, error) => {
+  console.error('Error:', error);
+  res.status(500).send('An error occurred while fetching data');
+};
+
+
+const endpoints = {
+  '/metrics': 'http://91.208.92.6:3000/',
+  '/api/stats': 'https://mainnet.mindscan.info/api/v2/stats',
+  '/api/txn': 'https://mainnet.mindscan.info/api/v2/main-page/transactions',
+  '/api/txn/chart': 'https://mainnet.mindscan.info/api/v2/stats/charts/transactions',
+  '/api/block': 'https://mainnet.mindscan.info/api/v2/main-page/blocks',
+};
+
+Object.entries(endpoints).forEach(([route, url]) => {
+  app.get(route, async (req, res) => {
+    try {
+      const response = await axios.get(url);
+      res.send(response.data);
+    } catch (error) {
+      handleError(res, error);
+    }
+  });
 });
 
 app.listen(port, () => {
   console.log(`Proxy server is running on port ${port}`);
 });
+
